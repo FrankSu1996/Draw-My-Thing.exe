465b4c131e5a14dc50d29a3fb63820898ff2cd0b
Frank Su <frank.su@ibm.com>
2022-09-18 18:05:35 -0400

    playing around with api

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,55 @@
 import "./App.css";
+import { useState, useRef } from "react";
+import CanvasDraw from "react-canvas-draw";
 
 function App() {
-  return <div className="App">Hello world from client!</div>;
+  const [loadCanvasData, setLoadCanvasData] = useState(null);
+  const saveableCanvas = useRef(null);
+  const loadableCanvas = useRef(null);
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          saveableCanvas.current.undo();
+        }}
+      >
+        Undo
+      </button>
+      <button
+        onClick={() => {
+          saveableCanvas.current.eraseAll();
+        }}
+      >
+        Erase
+      </button>
+      <button
+        onClick={() => {
+          console.log("saving canvas...");
+          localStorage.setItem(
+            "savedDrawing",
+            saveableCanvas.current.getSaveData()
+          );
+        }}
+      >
+        Save Canvas
+      </button>
+      <button
+        onClick={() => {
+          setLoadCanvasData(localStorage.getItem("savedDrawing"));
+        }}
+      >
+        Load Saved canvas
+      </button>
+      <CanvasDraw ref={saveableCanvas} canvasWidth={500} canvasHeight={500} />
+      <CanvasDraw
+        ref={loadableCanvas}
+        canvasWidth={500}
+        canvasHeight={500}
+        saveData={loadCanvasData}
+      />
+    </div>
+  );
 }
 
 export default App;
